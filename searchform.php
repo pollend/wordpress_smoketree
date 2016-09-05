d15ae5a15fccdeb05537084de67975156e09cffd
Michael Pollind <mpollind@gmail.com>
2016-09-05 12:15:29 -0700

    start
    start

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php echo home_url(); ?>" id="searchForm" method="get">
-        <label for="s" class="screen-reader-text">Search for:</label>
-        <input type="text" id="s" name="s" value="" />
-        
-        <input type="submit" value="" id="searchsubmit" />
-</form>
